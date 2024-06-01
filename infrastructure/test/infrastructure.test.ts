d0aff3fdbe3d5a156cad66544e1a4cb56d6e9e36
Scottie Enriquez <scottie@enriquez.xxx>
2024-05-31 21:29:28 -0700

    Updating GitHub Actions workflows

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,17 +1,12 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infrastructure from '../lib/infrastructure-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Infrastructure from '../lib/infrastructure-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/infrastructure-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  const app = new cdk.App();
+  const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    VisibilityTimeout: 300
+  });
 });
